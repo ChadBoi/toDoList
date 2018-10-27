a1903dea9352eb97d282a8c2aece6ed27ed0e28c
Chad Baxter <chaddudeguy@gmail.com>
2018-10-27 10:56:17 -0600

    format list buttons

--- todo.js
+++ todo.js
@@ -1,6 +1,17 @@
 
 let totalLists = 0;
 
+function deleteList(list){
+    $(list).parent().remove();
+}
+
+function clearCompleted(list){
+        for (let i = 0; i < list.length; i++){
+            $(`${list}:nth-child(i)`)
+        }
+}
+
+
 function addList(){
     $( ".container" ).append(`
     <div class="list${totalLists} list">
@@ -9,8 +20,13 @@ function addList(){
         <div class="item-container">
         
         </div>
-        <div class="deleteList">Delete</div>
+        <div class="clearCompleted" onclick="clearCompleted(this)"><span>Clear Completed</span></div>
+        <div class="deleteList" onclick="deleteList(this)"><span>Delete List</span></div>
     </div>
     `)
     totalLists++;
+}
+
+function addItem(list){
+
 }
