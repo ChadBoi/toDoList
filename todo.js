c540e51d2d5db90d4f5c2f96c28591a38de148b5
Chad Baxter <chaddudeguy@gmail.com>
2018-10-27 13:11:14 -0600

    debug addItem function

--- todo.js
+++ todo.js
@@ -11,28 +11,34 @@ function clearCompleted(list){
         }
 }
 
+function addItem(list){
+    console.log("item added");
+    let currentList = $(list).parent();
+    console.log(currentList);
+    let containedItems = $( currentList.children(".item-container") ).length + 1;
+    $( currentList.children(".item-container")).append(`
+    <div class="${currentList}item${containedItems} listItem"> 
+        <div class="editableListItem">
+            <input class="" placeholder="edit me">
+            <div class="updateTask" onclick="updateTask()"></div>
+        </div>
+    </div>
+    `);
+}
 
 function addList(){
     $( ".container" ).append(`
     <div class="list${totalLists} list">
         <div class="list-title">List ${totalLists+1}</div>
-        <div class="addListItem" onclick=addItem(this)">Add Item</div>
+        <div class="addListItem" onclick="addItem(this)">Add Item</div>
         <div class="item-container">
         
         </div>
         <div class="clearCompleted" onclick="clearCompleted(this)"><span>Clear Completed</span></div>
         <div class="deleteList" onclick="deleteList(this)"><span>Delete List</span></div>
     </div>
-    `)
+    `);
     totalLists++;
 }
 
-function addItem(list){
-    let currentList = list.parent();
-    let containedItems = $( "" + currentList + ":nth-child(3)" ).length + 1;
-    $( "" + currentList + ":nth-child(3)" ).append(`
-    <div class="${currentList}item${containedItems} listItem"> 
-        <input class="">
-    </div>
-    `);
-}
+
