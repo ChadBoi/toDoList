b80cd932740910504bde0f8c54358eab2c7721c4
Chad Baxter <chaddudeguy@gmail.com>
2018-10-27 17:48:03 -0600

    add edit functionality to listItem

--- todo.js
+++ todo.js
@@ -5,20 +5,42 @@ function deleteList(list){
     $(list).parent().remove();
 }
 
+function deleteItem(item){
+
+}
+
 function clearCompleted(list){
         for (let i = 0; i < list.length; i++){
             $(`${list}:nth-child(i)`)
         }
 }
 
+function updateTask(item){
+    if ($(item).parent("editableListItem")) {
+        let itemText = $(item).siblings(".itemInput").val();
+        $(item).parents().eq(1).append(`
+            <div class="staticListItem">
+                <div class="taskText">${itemText}</div>
+                <div class="updateTask" onclick="updateTask(this)"><i class="fas fa-check"></i></div>
+                <div class="deleteItem" onclick="deleteItem(this)"><i class="fas fa-trash-alt"></i></div>
+            </div>
+        `);
+    }
+}
+
+
+
+
+
 function addItem(list){
     let currentList = $(list).parent();
     let containedItems = $( currentList.children(".item-container") ).length + 1;
     $( currentList.children(".item-container")).append(`
-    <div class="${currentList}item${containedItems} listItem"> 
+    <div class="item${containedItems} listItem"> 
         <div class="editableListItem">
-            <input class="" placeholder="edit me">
-            <div class="updateTask" onclick="updateTask()"></div>
+            <input class="itemInput" placeholder="edit me">
+            <div class="updateTask" onclick="updateTask(this)"><i class="fas fa-check"></i></div>
+            <div class="deleteItem" onclick="deleteItem()"><i class="fas fa-trash-alt"></i></div>
         </div>
     </div>
     `);
