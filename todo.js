ac84a7537d1fa599e9870f76ddc64d109478af4c
Chad Baxter <chaddudeguy@gmail.com>
2018-10-28 20:42:39 -0600

    add clearCompleted function and general debug

--- todo.js
+++ todo.js
@@ -6,25 +6,51 @@ function deleteList(list){
 }
 
 function deleteItem(item){
+    $(item).parents(".listItem").remove();
+}
 
+function toggleComplete(item){
+    if ($(item).parents(".listItem").data("complete") === false){
+        $(item).parents(".listItem").data("complete", true);
+        $(item).css("color", "green");
+    }
+    else if($(item).parents(".listItem").data("complete") === true){
+        $(item).parents(".listItem").data("complete", false);
+        $(item).css("color", "black");
+    }
 }
 
 function clearCompleted(list){
-        for (let i = 0; i < list.length; i++){
-            $(`${list}:nth-child(i)`)
+
+    let currentList = $(list).siblings(".item-container");
+    let listLength = $(currentList).children().length;
+    for (let i = 0; i < listLength; i++){
+        if($(currentList).children(`.item${i}`).data("complete")){
+            $(currentList).children(`.item${i}`).remove();
         }
+    }
 }
 
 function updateTask(item){
-    if ($(item).parent("editableListItem")) {
+    if ($(item).parent(".editableListItem").length) {
         let itemText = $(item).siblings(".itemInput").val();
         $(item).parents().eq(1).append(`
             <div class="staticListItem">
                 <div class="taskText">${itemText}</div>
-                <div class="updateTask" onclick="updateTask(this)"><i class="fas fa-check"></i></div>
+                <div class="updateTask" onclick="updateTask(this)"><i class="fas fa-edit"></i></div>
+                <div class="markComplete" onclick="toggleComplete(this)"><i class="far fa-check-square"></i></div>
                 <div class="deleteItem" onclick="deleteItem(this)"><i class="fas fa-trash-alt"></i></div>
             </div>
         `);
+        $(item).parent().remove();
+    }
+    else{
+        $(item).parents().eq(1).append(`<div class="editableListItem">
+            <input class="itemInput" placeholder="${$(this).siblings(".taskText").text()}">
+            <div class="updateTask" onclick="updateTask(this)"><i class="fas fa-check"></i></div>
+            <div class="deleteItem" onclick="deleteItem(this)"><i class="fas fa-trash-alt"></i></div>
+        </div>`);
+        $(item).parent().remove();
     }
 }
 
@@ -33,14 +59,13 @@ function updateTask(item){
 
 
 function addItem(list){
-    let currentList = $(list).parent();
-    let containedItems = $( currentList.children(".item-container") ).length + 1;
-    $( currentList.children(".item-container")).append(`
-    <div class="item${containedItems} listItem"> 
+    let containedItems = $( list ).siblings(".item-container").children().length;
+    $( list).siblings(".item-container").append(`
+    <div class="item${containedItems} listItem" data-complete="false"> 
         <div class="editableListItem">
             <input class="itemInput" placeholder="edit me">
             <div class="updateTask" onclick="updateTask(this)"><i class="fas fa-check"></i></div>
-            <div class="deleteItem" onclick="deleteItem()"><i class="fas fa-trash-alt"></i></div>
+            <div class="deleteItem" onclick="deleteItem(this)"><i class="fas fa-trash-alt"></i></div>
         </div>
     </div>
     `);
